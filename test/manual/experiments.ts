3a1387fd98655319c676ded348046b3d831e2009
Tek <tek@inbox.ru>
2022-02-14 22:15:53 +0400

    ss

--- test/manual/experiments.ts
+++ test/manual/experiments.ts
@@ -3,9 +3,9 @@ import { FpsMeter } from '../../src/lib/FpsMeter';
 export class Experiments {
   public addHelloToBody() {
     const fp1 = new FpsMeter({ theme: 'light', position: 'topLeft' });
-    // const fp2 = new FpsMeter({ theme: 'dark' });
-    // const fp3 = new FpsMeter({ theme: 'colorful', position: 'bottomLeft' });
-    // const fp4 = new FpsMeter({ theme: 'transparent', position: 'bottomRight' });
+    const fp2 = new FpsMeter({ theme: 'dark' });
+    const fp3 = new FpsMeter({ theme: 'colorful', position: 'bottomLeft' });
+    const fp4 = new FpsMeter({ theme: 'transparent', position: 'bottomRight' });
   }
 }
 
